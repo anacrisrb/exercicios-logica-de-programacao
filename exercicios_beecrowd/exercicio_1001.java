750b0cc0bcc2d1982968725402c9ae6da3734c53
AnaCristina <anacrisribeirost@gmail.com>
2025-08-15 16:57:52 -0300

    Exercicio_1001

---
+++ exercicios_beecrowd/exercicio_1001.java
@@ -0,0 +1,22 @@
+package exercicios_beecrowd;
+
+import java.util.Scanner;
+
+public class exercicio_1001 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int x = A + B;
+		
+		System.out.println("X = " + x);
+		
+		
+		sc.close();
+	}
+
+}
